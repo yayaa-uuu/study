b4fecf2f15abfa87f9c0d14c4ba7b20775e421fa
yy <240839670@qq.com>
2022-04-07 23:54:11 +0800

    spring   demo

--- spring/src/main/java/com/wx/Application.java
+++ spring/src/main/java/com/wx/Application.java
@@ -1,15 +1,13 @@
 package com.wx;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("com.wx.demo.dao")
-public class IdempotentApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(IdempotentApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 
