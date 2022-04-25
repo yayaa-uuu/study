a7c11dbed231584c8e9a76db7853b50d73f7a3d6
yy <240839670@qq.com>
2022-04-25 10:44:58 +0800

    iterator fast fail

---
+++ java8/src/main/java/com/wx/collection/HiddenIterator.java
@@ -0,0 +1,32 @@
+package com.wx.collection;
+
+import java.util.*;
+
+public class HiddenIterator {
+    private final Set<Integer> set=new HashSet<>();
+//    private final Set<Integer> set= Collections.synchronizedSet(new HashSet<>());
+    public synchronized void add(Integer i){
+        set.add(i);
+    }
+    public synchronized void remove(Integer i) {
+        set.remove(i);
+    }
+    public  void addTenThings(){
+        Random r=new Random();
+        for (int i = 0; i < 10; i++) {
+            add(r.nextInt());
+            System.out.println("debug : added ten elements to "+ set);      //
+        }
+    }
+
+    public static void main(String[] args) {
+        HiddenIterator hiddenIterator = new HiddenIterator();
+        new Thread(()->{
+            hiddenIterator.addTenThings();
+        }).start();
+        new Thread(()->{
+            hiddenIterator.addTenThings();
+        }).start();
+    }
+
+}
