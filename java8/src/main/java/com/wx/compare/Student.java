f3bd7d46accb01b8d23d77f08da0dde83a790e5b
yy <240839670@qq.com>
2022-05-13 13:01:15 +0800

    compare

---
+++ java8/src/main/java/com/wx/compare/Student.java
@@ -0,0 +1,33 @@
+package com.wx.compare;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author wxli
+ * @date 2021/7/21 10:24
+ */
+@Data
+@AllArgsConstructor
+public class Student {
+    private String name;
+    private String age;
+}
+class MaxAge{
+    public static void main(String[] args) {
+
+
+        List<Student> list=new ArrayList<>();
+        list.add(new Student("zhangsan","19"));
+        list.add(new Student("lisi","20"));
+        list.add(new Student("wangmazi","-123"));
+        System.out.println(list.stream().max(MaxAge::compare).get());
+    }
+
+    private static int compare(Student o1, Student o2) {
+        return o1.getAge().compareTo(o2.getAge());
+    }
+}
