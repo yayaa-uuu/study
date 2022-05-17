7384358330556c8a34cf0dc4254f9520ce15ffd9
yy <240839670@qq.com>
2022-05-17 23:57:23 +0800

    5.17算法入门

--- algorithm/src/main/java/com/wx/niuke/ScannerDemo.java
+++ algorithm/src/main/java/com/wx/niuke/ScannerDemo.java
@@ -1,5 +1,6 @@
-```java
+package com.wx.niuke;
 
+import java.util.Scanner;
 
 public class ScannerDemo {
     public static void main(String[] args) {
@@ -13,18 +14,3 @@ public class ScannerDemo {
         }
     }
 }
-
-
-//空格和换行敏感
-sc.hasNext();         //判断是否有字符
-sc.nextInt();         //获取一个整数读到空格或者换行结束
-sc.next();       //读取str读到空格或者换行结束
-
-//空格不敏感
-sc.hasNextLine();     //是否有换行   /n
-sc.nextLine();        //读取一整行到/n结束
-```
-
-
-#### 参考
-[练习题](https://www.nowcoder.com/test/27976983/summary#question)
--- algorithm/src/main/java/com/wx/sort/Sort.java
+++ algorithm/src/main/java/com/wx/sort/Sort.java
@@ -10,7 +10,10 @@ public class Sort {
     public void bubbleSort(int[] a) {
         for (int i = 0; i < a.length - 1; i++) {
             for (int j = 0; j < a.length - 1 - i; j++) {
+                //比较前后两个数
+                //前面大于后面交换
                 if (a[j] > a[j + 1]) {
+                    //交换
                     int temp = a[j];
                     a[j] = a[j + 1];
                     a[j + 1] = temp;
