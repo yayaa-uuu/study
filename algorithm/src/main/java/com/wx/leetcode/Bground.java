21ef8c2bc8e632dcc68e62648f82fc9fcd361fbb
yy <240839670@qq.com>
2022-06-16 01:04:48 +0800

    ------

---
+++ algorithm/src/main/java/com/wx/leetcode/Bground.java
@@ -0,0 +1,13 @@
+package com.wx.leetcode;
+
+public class Bground extends Thread{
+    public static void main(String argv[]){
+        Bground b = new Bground();
+        b.run();
+    }
+    public void start(){
+        for(int i=0;i<10;i++){
+            System.out.println("Value of i = "+i);
+        }
+    }
+}
