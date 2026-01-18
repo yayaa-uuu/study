d74edf6ba433c5d99f7dc88f05eb4a183904efaf
yy <yy240839670@gmail.com>
2026-01-18 14:55:05 +0800

    canal demo

---
+++ demo/src/main/java/com/wxli/Main.java
@@ -0,0 +1,16 @@
+package com.wxli;
+
+import java.util.*;
+
+
+
+public class Main {
+    public static void main(String[] args) {
+        String s = "cbaebabacd", p = "abc";
+
+    }
+}
+
+class Solution {
+
+}
